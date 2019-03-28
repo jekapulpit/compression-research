3673097cf1a438cdaf90177a29d4371b42204874
Eugene <q010@bk.ru>
2019-03-28 23:04:43 +0300

    first commit

---
+++ compress-research/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace compress_research
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
