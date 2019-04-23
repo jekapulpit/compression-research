6a6b0d1ebf2ba4f650cac9665d07972bf616b73c
Eugene <q010@bk.ru>
2019-04-23 22:28:59 +0300

    added RLE compressor

--- compress-research/Program.cs
+++ compress-research/Program.cs
@@ -6,15 +6,22 @@ using System.Threading.Tasks;
 
 namespace compress_research
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
+            string input = "ddddfdsfdd;fds;kfjddsajhbkjfdsbgkjfdbgfdlgnfdslkjdsfg;dsjkgdmnbv;ldsmgh;ldsjgdsjgdspoidsjgtpodnpishgpodsjgdspogjdspgjdsgpo[dsjgdspojgdspogdsjgpodsgjspodjgsp";
             BWT sorter = new BWT();
-            string encryptMessage = sorter.Encrypt("scpsosat");
-            Console.WriteLine("Encrypt message : " + encryptMessage + "; number : " + (sorter.Number + 1));
-            string decryptMessage = sorter.Decrypt(encryptMessage, sorter.Number);
-            Console.WriteLine("Decrypt message : " + decryptMessage);
+            string encryptMessage = sorter.Encrypt(input);
+            RLE compressor = new RLE();
+            Console.WriteLine("Supreme message : " + input);
+            Console.WriteLine("Sorted message : " + encryptMessage);
+            encryptMessage = compressor.encode(encryptMessage);
+            Console.WriteLine("Encrypted message : " + encryptMessage);
+            string decryptMessage = compressor.decode(encryptMessage);
+            Console.WriteLine("Decrypted message : " + decryptMessage);
+            decryptMessage = sorter.Decrypt(decryptMessage, sorter.Number);
+            Console.WriteLine("Resorted message : " + decryptMessage);
             Console.ReadKey();
         }
     }
---
+++ compress-research/RLE.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace compress_research
+{
+    class RLE
+    {
+        public string encode(string s)
+        {
+            StringBuilder sb = new StringBuilder();
+            int count = 1;
+            char current = s[0];
+            for (int i = 1; i < s.Length; i++)
+            {
+                if (current == s[i])
+                {
+                    count++;
+                }
+                else
+                {
+                    sb.AppendFormat("{0}{1}", count, current);
+                    count = 1;
+                    current = s[i];
+                }
+            }
+            sb.AppendFormat("{0}{1}", count, current);
+            return sb.ToString();
+        }
+
+        public string decode(string s)
+        {
+            string a = "";
+            int count = 0;
+            StringBuilder sb = new StringBuilder();
+            char current = char.MinValue;
+            for (int i = 0; i < s.Length; i++)
+            {
+                current = s[i];
+                if (char.IsDigit(current))
+                    a += current;
+                else
+                {
+                    count = int.Parse(a);
+                    a = "";
+                    for (int j = 0; j < count; j++)
+                        sb.Append(current);
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
